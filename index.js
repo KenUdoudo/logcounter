3c73b67950d97dc2e867e16289286bbebb7d8558
KenUdoudo <101623697+KenUdoudo@users.noreply.github.com>
2022-11-10 17:31:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ let eMail = document.getElementsByClassName('eMail')
 let myForm = document.getElementsByClassName('myForm')
 
 function submit(){
+  myForm.preventDefault();
   
+  if(userName.value === "" || eMail.value === ""){
+
+    }
 }
